0b95af1c3f74b580646d28107986913b4a6cff35
losrobbos <losrobbos@web.de>
2023-01-19 10:43:17 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,23 @@
-import { useState } from 'react'
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Events } from './pages/Events'
+import { Fruits } from './pages/Fruits'
 
 function App() {
 
-  // state for fruit DATA
-  const [fruitStrings, setFruitStrings] = useState(["apple", "banana", "cherry"])
-  const [fruitObjects, setFruitObjects] = useState([
-    { _id: "f1", name: "apple" },
-    { _id: "f2", name: "banana" },
-    { _id: "f3", name: "cherry" },
-  ])
-
-  // state for holding NEW items
-  const [fruitStringNew, setFruitStringNew] = useState("")
-  const [fruitObjectNew, setFruitObjectNew] = useState({ _id: Math.random(), name: "" })
-
-  const onAddFruitFlat = () => {
-
-  }
-
-  const onAddFruitObject = () => {
-
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <h2>Add Fruit (string)</h2>
-          <input type="text" name="name" />
-          <button>Add</button>
-        </div>
-        <div>
-          <h2>Add Fruit (object)</h2>
-          <input type="text" name="name" />
-          <button>Add</button>
-        </div>
-        // TODO: map through fruit strings and show
-        <div className="fruits-flat"></div>
-        // TODO: map through fruit objects and show
-        <div className="fruits-objects"></div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <NavLink to={"/"} >Home</NavLink>
+        <NavLink to={"/fruits"} >Fruits</NavLink>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Events />} />
+        <Route path='/fruits' element={<Fruits />} />
+      </Routes>
+    </BrowserRouter>
   )
+
 }
 
 export default App
---
+++ src/pages/Fruits.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+
+export function Fruits() {
+
+  // state for fruit DATA
+  const [fruitStrings, setFruitStrings] = useState(["apple", "banana", "cherry"])
+  const [fruitObjects, setFruitObjects] = useState([
+    { _id: "f1", name: "apple" },
+    { _id: "f2", name: "banana" },
+    { _id: "f3", name: "cherry" },
+  ])
+
+  // state for holding NEW items
+  const [fruitStringNew, setFruitStringNew] = useState("")
+  const [fruitObjectNew, setFruitObjectNew] = useState({ _id: Math.random(), name: "" })
+
+  const onAddFruitFlat = () => {
+
+  }
+
+  const onAddFruitObject = () => {
+
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Data Add Training</h2>
+        <div>
+          <h3>Add Fruit (string)</h3>
+          <input type="text" name="name" />
+          <button>Add</button>
+        </div>
+        <div>
+          <h3>Add Fruit (object)</h3>
+          <input type="text" name="name" />
+          <button>Add</button>
+        </div>
+
+        {/* TODO: map through fruit strings and show */}
+        <div className="fruits-flat">
+          
+        </div>
+        {/* TODO: map through fruit objects and show */}
+        <div className="fruits-objects">
+          
+        </div>
+      </header>
+    </div>
+  )
+
+}

6c2dbd6bbab241aad8e3b5767323a2f57ce4142d
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-23 16:10:23 +0100

    Events

--- src/pages/Change.tsx
+++ src/pages/Change.tsx
@@ -1,21 +1,19 @@
 export function Change() {
-
-  const changeName: React.ChangeEventHandler<HTMLInputElement> = () => {
-
-  }
+  const changeName: React.ChangeEventHandler<HTMLInputElement> = () => {};
 
   return (
     <div>
       <h2>Basic Events</h2>
 
       <div>
-        <label>Name: </label><input type="text" onChange={changeName} autoComplete="off" />
+        <label>Name: </label>
+        <input type="text" onChange={changeName} autoComplete="off" />
       </div>
 
       <div>
-        <label>Name: </label><input type="text" onChange={(e) => changeName(e)} autoComplete="off" />
+        <label>Name: </label>
+        <input type="text" onChange={(e) => changeName(e)} autoComplete="off" />
       </div>
     </div>
-  )
-
+  );
 }
--- src/pages/Fruits.tsx
+++ src/pages/Fruits.tsx
@@ -1,50 +1,82 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export function Fruits() {
-
   // state for fruit DATA
-  const [fruitStrings, setFruitStrings] = useState(["apple", "banana", "cherry"])
+  const [fruitStrings, setFruitStrings] = useState([
+    "apple",
+    "banana",
+    "cherry",
+  ]);
   const [fruitObjects, setFruitObjects] = useState([
     { _id: "f1", name: "apple" },
     { _id: "f2", name: "banana" },
     { _id: "f3", name: "cherry" },
-  ])
+  ]);
 
   // state for holding NEW items
-  const [fruitStringNew, setFruitStringNew] = useState("")
-  const [fruitObjectNew, setFruitObjectNew] = useState({ _id: Math.random(), name: "" })
+  const [fruitStringNew, setFruitStringNew] = useState("");
+  const [fruitObjectNew, setFruitObjectNew] = useState({
+    _id: Math.random().toString(),
+    name: "",
+  });
 
   const onAddFruitFlat = () => {
+    setFruitStrings([...fruitStrings, fruitStringNew]);
+    setFruitStringNew("");
+  };
 
-  }
+  const handleFruitObjectChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFruitObjectNew({ ...fruitObjectNew, name: e.target.value });
+  };
 
   const onAddFruitObject = () => {
-
-  }
+    setFruitObjects([...fruitObjects, fruitObjectNew] )
+    setFruitObjectNew({...fruitObjectNew, _id: Date.now().toString(), name:""})
+  };
 
   return (
     <div>
       <h2>Data Add Training</h2>
+      {/* STRING */}
       <div>
-        <h3>Add Fruit (string)</h3>
-        <input type="text" name="name" autoComplete="off" />
-        <button>Add</button>
+        <h3>Add Fruit "STRING"</h3>
+        <input
+          type="text"
+          name="name"
+          autoComplete="off"
+          value={fruitStringNew}
+          onChange={(e) => setFruitStringNew(e.target.value)}
+        />
+        <button onClick={onAddFruitFlat}>Add</button>
       </div>
-      <div>
-        <h3>Add Fruit (object)</h3>
-        <input type="text" name="name" autoComplete="off"/>
-        <button>Add</button>
-      </div>
-
       {/* TODO: map through fruit strings and show */}
       <div className="fruits-flat">
-
+        {fruitStrings.map((fruitString) => (
+          <div key={fruitString} className="str">
+            {fruitString}
+          </div>
+        ))}
+      </div>
+      {/* OBJECT */}
+      <div>
+        <h3>Add Fruit "OBJECT"</h3>
+        <input
+          type="text"
+          name="name"
+          autoComplete="off"
+          value={fruitObjectNew.name}
+          onChange={handleFruitObjectChange}
+        />
+        <button onClick={onAddFruitObject}>Add</button>
       </div>
       {/* TODO: map through fruit objects and show */}
       <div className="fruits-objects">
-
+        {fruitObjects.map((fruit) => (
+          <div key={fruit._id} className="name">
+            {fruit.name}
+          </div>
+        ))}
       </div>
     </div>
-  )
-
+  );
 }
